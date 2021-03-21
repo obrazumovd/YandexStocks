828f3dc2be18e6baeb2a9e5820f961feec4e9453
Dmitriy Obrazumov <obrazumovd@mail.ru>
2021-03-21 19:32:15 +0300

    Initial Commit

---
+++ YandexStocks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YandexStocks
+//
+//  Created by Дмитрий on 21.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
